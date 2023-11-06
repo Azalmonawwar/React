e3207dfd2bd0b569624d128aa9384c4688974795
Azalmonawwar <monawwarazal@gmail.com>
2023-11-06 22:37:25 +0530

    a basic todo list app

---
+++ 01_react-hooks/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+
+
+
+
+function App() {
+  const [formData, setFormData] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const addTodo = () => {
+    if (formData === '') {
+      alert('Please enter a todo')
+      return
+    }
+
+    setTodos([...todos, formData])
+    localStorage.setItem(`list${formData}`, formData)
+    setFormData('')
+  }
+
+  const removeTodo = () => {
+    todos.pop()
+    localStorage.removeItem(`list${todos}`)
+    setTodos([...todos])
+  }
+  return (
+    <>
+      <h1 className='text-red-300 underline'>
+        Todo list
+      </h1>
+      <div>
+        <input type="text" onChange={(e) => setFormData(e.target.value)} value={formData} />
+        <button onClick={addTodo}>Add</button>
+      </div>
+
+      {/* list of all todos  */}
+      <ul>
+        {
+          todos.map((todo) => {
+            return (
+              <div key={todo} className='flex gap-4 justify-between my-2'>
+                <li >
+                  {todo}
+                </li>
+                <button className='px-4 py-2 bg-black text-white' onClick={removeTodo}>Remove Todo</button>
+              </div>
+            )
+          })
+        }
+
+      </ul>
+
+    </>
+  )
+}
+
+export default App
